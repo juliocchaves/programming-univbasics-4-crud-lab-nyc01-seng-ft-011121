d7d8bad6faa7d314a2ed282d8cdde1ac90142af7
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-29 02:30:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,8 +24,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  ["hello", "welcome"]
-  retrieve_element_from_index[1]
+  ["hello", "welcome"][1]
 end
 
 def retrieve_first_element_from_array(array)
