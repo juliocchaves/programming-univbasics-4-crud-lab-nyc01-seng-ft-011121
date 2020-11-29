b1d5b7ade645f357e44fcefc4b92e7298d5d6dfe
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-29 02:26:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,31 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["apple", "banana", "cherry", "pear"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["mary"] << ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ["hello"].unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ["hello", "arrays!"].pop
 end
 
 def remove_element_from_start_of_array(array)
+  ["wow", "bye", "city"].shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  ["hello", "bye", "welcome"]
+  retrieve_element_from_index[1]
 end
 
 def retrieve_first_element_from_array(array)
